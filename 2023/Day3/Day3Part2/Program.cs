f1d8e215349534c28f3552fa9c80a6f903c7497c
Marcin van de Ven <marcinvandeven@gmail.com>
2023-12-05 12:07:14 +0100

    Added Day 3 project files

---
+++ 2023/Day3/Day3Part2/Program.cs
@@ -0,0 +1,29 @@
+﻿ConsoleColor startColor = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("This is a Advent Of Code solution application created by:");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Marcin van de Ven (@marzman95)");
+Console.ForegroundColor = startColor;
+Console.WriteLine("----------------------------------------------");
+
+string inputFile = $"{{INPUTFILE}}";
+List<string> lines = new List<string>();
+List<int> sums = new List<int>();
+FileStream _stream = new FileStream(inputFile, FileMode.Open, FileAccess.Read);
+StreamReader _reader = new StreamReader(_stream);
+int lineCount = 0;
+int totalScore = 0;
+
+Console.WriteLine($"Reading input from {_stream.Name}");
+// Read input lines into memory
+for (int i = 0; !_reader.EndOfStream; i++)
+{
+    lines.Add(_reader.ReadLine());
+}
+lineCount = lines.Count;
+Console.WriteLine($"Read {lineCount} lines of input from {_stream.Name}");
+
+foreach (string line in lines)
+{
+    totalScore += 1;
+}
